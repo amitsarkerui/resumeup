70782df492e94a5f8449ebce3fc3ed720dafb6ed
Amit Sarker <amitsarkerstock@gmail.com>
2025-02-18 16:57:18 +0600

    Footer added

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -170,6 +170,32 @@
       </div>
     </div>
   </section>
+  <!-- Footer -->
+
+  <footer
+    class="w-full p-4 bg-white border-t border-gray-200 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
+  >
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+      >© 2025 <a href="#" class="hover:underline">Resume</a>. All Rights
+      Reserved.
+    </span>
+    <ul
+      class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
+    >
+      <li>
+        <a href="#" class="hover:underline me-4 md:me-6">About</a>
+      </li>
+      <li>
+        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+      </li>
+      <li>
+        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+      </li>
+      <li>
+        <a href="#" class="hover:underline">Contact</a>
+      </li>
+    </ul>
+  </footer>
 </template>
 
 <script>
